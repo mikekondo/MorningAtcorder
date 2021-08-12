7b2bfc0aa4a9a6c7c292317c4526b1a7793de6cb
mikekondo <foad5969@gmail.com>
2021-08-12 21:03:33 +0900

    secondCommit

---
+++ basic2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+using namespace std;
+int main()
+{
+    cout << "test" << endl;
+}
