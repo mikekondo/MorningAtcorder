542db79a0c90ea7d4c9df5a0adcba7e00c449798
近藤米功 <foad5969@gmail.com>
2021-08-15 16:12:16 +0900

    add 214

---
+++ B-214.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+using namespace std;
+int main()
+{
+    int s, t;
+    cin >> s >> t;
+    int count = 0;
+    for (int a = 0; a <= s; a++)
+    {
+        for (int b = 0; b <= s; b++)
+        {
+            for (int c = 0; c <= s; c++)
+            {
+                if (a + b + c <= s && a * b * c <= t)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    cout << count << endl;
+}
