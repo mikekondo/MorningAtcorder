4427c8a976d68a205b36a3ffc2c088523dc86eb2
近藤米功 <foad5969@gmail.com>
2021-09-27 08:48:58 +0900

    update

--- B/220.cpp
+++ B/220.cpp
@@ -11,22 +11,20 @@ int main()
 {
     int k, a, b;
     cin >> k >> a >> b;
-    int ary_a = a;
-    int ary_b = b;
     int ans_a = 0;
     int ans_b = 0;
     int base_a = 1;
     int base_b = 1;
-    while (ary_a > 0)
+    while (a > 0)
     {
-        ans_a = ans_a + (ary_a % 10) * base_a;
-        ary_a /= 10;
+        ans_a = ans_a + (a % 10) * base_a;
+        a /= 10;
         base_a *= k;
     }
-    while (ary_b > 0)
+    while (b > 0)
     {
-        ans_b = ans_b + (ary_b % 10) * base_b;
-        ary_b /= 10;
+        ans_b = ans_b + (b % 10) * base_b;
+        b /= 10;
         base_b *= k;
     }
     long long ans = ans_a * ans_b;
